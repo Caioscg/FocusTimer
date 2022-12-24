8032521a475525f09d1a4c2d7a8b2ca8e8635000
Caioscg <caio.gonzaga@discente.ufg.br>
2022-12-23 21:11:36 -0300

    es6 modules

--- js/main.js
+++ js/main.js
@@ -1,44 +1,4 @@
-//*** Clock and stop button ***//
-
-const stop = document.querySelector(".stop")
-const clock = document.querySelector(".clock")
-
-const minutesDisplay = document.querySelector(".minutes")
-const secondsDisplay = document.querySelector(".seconds")
-
-let timerPrompt = Number(minutesDisplay.textContent)
-let timerTimeOut
-
-function updateTimer(minutes, seconds) {
-    minutesDisplay.textContent = String(minutes).padStart(2, "0") // string com 2 caracteres, preenchido com 0 antes (se ja não tiver 2)
-    secondsDisplay.textContent = String(seconds).padStart(2, "0")
-}
-
-function resetTimer() {
-    updateTimer(timerPrompt, 0)
-
-    clearTimeout(timerTimeOut)
-}
-
-stop.addEventListener("click", () => {
-    resetControls()
-
-    resetTimer()
-})
-
-clock.addEventListener("click", () => {
-    timerPrompt = prompt("Digite a quantidade de minutos:") || 0
-
-    updateTimer(timerPrompt, 0)
-})
-
-function resetControls() {
-    play.classList.remove('hide')
-    pause.classList.add('hide')
-
-    clock.classList.remove('hide')
-    stop.classList.add("hide")
-}
+import {clock, stop, minutesDisplay ,secondsDisplay, timerTimeOut, updateTimer, resetControls} from './clock.js'
 
 //*** Clock timer ***//
 
